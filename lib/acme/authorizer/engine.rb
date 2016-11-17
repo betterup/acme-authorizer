b3b4d9cb48cf8f0e52861226d49ebf585e356b8f
Lars Klevan <lars@awesome.ai>
2016-11-17 09:48:18 -0600

    exclude acme-authorizer path from force_ssl

--- lib/acme/authorizer/engine.rb
+++ lib/acme/authorizer/engine.rb
@@ -1,11 +1,9 @@
 module Acme
   module Authorizer
     class Engine < ::Rails::Engine
-      InvalidConfigurationError = Class.new(StandardError)
-
       isolate_namespace Acme::Authorizer
-      initializer 'acme-authorizer.config.force_ssl_assertion' do |app|
-        raise InvalidConfigurationError, 'force_ssl can not be enabled globally. see http://guides.rubyonrails.org/action_controller_overview.html#force-https-protocol' if app.config.force_ssl
+      initializer 'acme-authorizer.config.set_ssl_options' do |app|
+        app.config.ssl_options.deep_merge! redirect: { exclude: -> request { request.path =~  /\A\/\.well-known\/acme-challenge/ } }
       end
     end
   end
